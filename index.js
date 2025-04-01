5f46256bd0423d267824c6808febe16b1dd09f56
baqarzafarfarooqui <baqarzafarfarooqui2021@gmail.com>
2025-04-01 16:50:06 +0500

    myfirst

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function foo(n) {
-    if (n <= 1) {
-        return 1;  // Base case: factorial(1) = 1
-    } else {
-        return n * foo(n - 1);  // Recursive call
-    }
+// function foo(n) {
+//     if (n <= 1) {
+//         return 1;  // Base case: factorial(1) = 1
+//     } else {
+//         return n * foo(n - 1);  // Recursive call
+//     }
+// }
+
+// console.log(foo(5)); // Output: 120
+
+
+
+function foo2(array){
+   
+    for(let i = 0 ; i <=array.length ; i++){
+        let pivot = array[0]
+
+        if( array[i] > pivot)   {
+               
+           array [array[i],pivot] = [pivot , array[i]]
+
+
+
+
+         console.log(array)
+      
+        
+      
+          
+ }   }
+
+
+
 }
 
-console.log(foo(5)); // Output: 120
+console.log(foo2([20,50,30 ,10]))
